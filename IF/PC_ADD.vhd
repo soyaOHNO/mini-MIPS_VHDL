833b55c66ac80bdabdf8b9f90101b4beebbeec40
soyaOHNO <souya1683@gmail.com>
2026-02-18 15:26:47 +0900

    first commit

---
+++ IF/PC_ADD.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_arith.all;
+
+entity PC_ADD is port
+(
+	PC_cur	: in integer;
+	PC_next	: out integer;
+);
+end PC;
+
+architecture behavior of PC_ADD is
+begin
+	process(RST, PC_cur)
+	begin
+		if (RST'event and RST='1')then
+			PC_next <= 0;
+		else
+			PC_next <= PC_cur;
+		end if;
+	end process;
+end behavior;
